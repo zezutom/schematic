bb2a81b0b8f545cf77c23c3613adaf694f81b81f
tomaszezula <tomas.zezula@panaseer.com>
2017-02-26 21:42:27 +0000

    ISSUE-1: Added a runner script.

---
+++ runner.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+#
+# Starts the server user either a default config or the provided one.
+#
+# Usage: sh runner.sh -j path/to/the/jar [-c /path/to/your/config/file]
+#
+# Arguments:
+#
+# -j path to the jar file       ex.: ~/schematic/schematic-0.1.0.jar
+# -c path to the config file    ex.: ~/schematic/custom.properties
+#
+
+function usage {
+    echo "Usage: -j path/to/the/jar [-c /path/to/your/config/file]"
+}
+
+while getopts ":j:c:" opt; do
+    case ${opt} in
+        j) JAR="$OPTARG"
+        ;;
+        c) CONFIG_FILE="$OPTARG"
+        ;;
+        \?) usage
+        ;;
+    esac
+done
+
+if [ -z ${JAR+x} ]; then
+    usage
+    exit 1
+fi
+
+if [ -z ${CONFIG_FILE+x} ]; then
+    java -jar ${JAR} org.zezutom.schematic.App
+else
+    java -jar ${JAR} -Dspring.config.location=${CONFIG_FILE} org.zezutom.schematic.App
+fi
+
